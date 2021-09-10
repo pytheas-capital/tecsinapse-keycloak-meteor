cf64afb238db612d6a02d6a58bb88a2ec346a17d
Eric Taïeb Walch <48756870+teknologista@users.noreply.github.com>
2021-09-10 10:52:55 +0200

    fix: update for newer meteor

--- package.js
+++ package.js
@@ -4,18 +4,18 @@ Npm.depends({
 });
 
 Package.describe({
-  name: 'tecsinapse:keycloak-meteor',
-  version: '0.0.17',
+  name: 'pytheas:keycloak-meteor',
+  version: '0.0.18',
   summary: 'Keycloak login integrated with Accounts',
-  git: 'https://github.com/tecsinapse/tecsinapse-keycloak-meteor.git',
+  git: 'https://github.com/pytheas-capital/tecsinapse-keycloak-meteor.git',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
   api.versionsFrom('1.4');
   api.use('ecmascript');
-  api.use('service-configuration@1.0.11');
-  api.use('accounts-base');
+  api.use('service-configuration@1.1.0');
+  api.use('accounts-base@2.0.0');
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
 });
